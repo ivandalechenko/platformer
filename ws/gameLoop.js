b4e06e1de9e6278504b3151597b2e0af826b26ce
ivandalechenko <ivandalechenko@gmail.com>
2025-05-13 19:50:09 +0300

    fix

--- ws/gameLoop.js
+++ ws/gameLoop.js
@@ -4,28 +4,53 @@ const { connections } = require('./serverController');
 const physics = require('./physics');
 const { messageToAll } = require('./wsUtils');
 
-const tps = 30;
+let tiks = []
+
+const addTik = () => {
+    const now = Date.now();
+    tiks.push(now);
+    tiks = tiks.filter(t => now - t <= 1000);
+}
 
 function gameLoop() {
-    setInterval(() => {
-        for (const name in connections) {
-            physics.applyControls(name, connections[name].buttons);
+    const tps = 30;
+    const tickInterval = 1000 / tps;
+    let lastTick = Date.now();
+
+    function loop() {
+        const now = Date.now();
+        const delta = now - lastTick;
+
+        if (delta >= tickInterval) {
+            lastTick = now;
+
+            addTik();
+            console.log(`tiks: ${tiks.length}`, 'tick', now, 'Δ', delta);
+
+            for (const name in connections) {
+                physics.applyControls(name, connections[name].buttons);
+            }
+
+            physics.updatePhysics(tickInterval);
+
+            const players = {};
+            for (const name in connections) {
+                const s = physics.getPlayerState(name);
+                if (s) players[name] = s;
+            }
+
+            const map = physics.getMapState();
+
+            messageToAll(connections, {
+                type: 'update',
+                data: { timestamp: now, players, map }
+            });
         }
 
-        physics.updatePhysics(1000 / tps);
+        setImmediate(loop);
+    }
 
-        const players = {};
-        for (const name in connections) {
-            const s = physics.getPlayerState(name);
-            if (s) players[name] = s;
-        }
-        const map = physics.getMapState();
-
-        messageToAll(connections, {
-            type: 'update',
-            data: { timestamp: Date.now(), players, map }
-        });
-    }, 1000 / tps);
+    loop();
 }
 
 module.exports = { gameLoop };
